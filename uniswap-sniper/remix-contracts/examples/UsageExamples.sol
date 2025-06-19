f392701cea0c5974bf4cc734a0a6c74caa17e1f9
Todd Tan <todd.tan@capacity.com>
2025-06-18 19:01:54 -0500

    Fix compile errors

--- uniswap-sniper/remix-contracts/examples/UsageExamples.sol
+++ uniswap-sniper/remix-contracts/examples/UsageExamples.sol
@@ -15,7 +15,7 @@ contract UsageExamples {
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     
-    constructor(address _sniperAddress) {
+    constructor(address payable _sniperAddress) {
         sniper = UniswapSniper(_sniperAddress);
     }
     
